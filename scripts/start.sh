24d56fb27b051c316af5198f9d7440643d876564
EC2 Default User <ec2-user@ip-172-31-10-36.us-west-2.compute.internal>
2017-12-06 08:20:26 +0000

    update config

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,10 @@ export JAVA_HOME=/usr
 export JRE_HOME=/usr
 export PATH=$PATH:/usr/bin
 
-nohup java -Xmx1024m -Xms1024m -jar devops-workshop-0.0.1-SNAPSHOT.jar 2>&1 >> /home/ec2-user/javaapp/log/log.txt &
+#nohup java -Xmx1024m -Xms1024m -jar devops-workshop-0.0.1-SNAPSHOT.jar 2>&1 >> /home/ec2-user/javaapp/log/log.txt &
+aws s3 cp s3://aws-workshop2017-us-west-2-793169309996-project-1/config/application.properties .
+nohup java -Xmx1024m -Xms1024m -Dspring.config.location=./application.properties \
+    -jar devops-workshop-0.0.1-SNAPSHOT.jar 2>&1 >> /home/ec2-user/javaapp/log/log.txt &
 
 echo "Started application..."
 
